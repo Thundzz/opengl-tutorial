08caa8172c72f5bcf36fd56ca2eeee38b270263b
Thundzz <yacinenew@gmail.com>
2018-03-17 23:54:03 +0100

    add glm and implement loading a mesh to the GPU

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,26 @@
 #include "Display.h"
 #include "SDL2/SDL.h"
 #include "shader.h"
+#include "mesh.h"
+#include "glm/glm.hpp"
 
 int main(int argc, char *argv[])
 {
 	Display display(800,600, "my window");
 	Shader shader("../res/basicShader");
+	Vertex vertices[] = {
+		Vertex(glm::vec3(-0.5, -0.5, 0)),
+		Vertex(glm::vec3(0, 0.5, 0)),
+		Vertex(glm::vec3(0.5, -0.5, 0))
+	};
+
+	Mesh mesh(vertices, sizeof(vertices)/sizeof(vertices[0]));
+
 
 	while(!display.isClosed()){
 		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
 		shader.Bind();
-
+		mesh.Draw();
 		display.Update();
 	}
 
